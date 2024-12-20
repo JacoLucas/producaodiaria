cc69e3b234db8484da3a3546a0ffa56263c78d8d
Lucas jacomel <lucas.jacomel@pucpr.edu.br>
2024-12-20 14:24:27 -0300

    Update Controle_de_Produção.py

--- Controle_de_Produção.py
+++ Controle_de_Produção.py
@@ -171,19 +171,17 @@ def update_graphs_and_table(selected_atividade, selected_obra, selected_mes, sel
     final_prev_values = {key: combined_summary[key].dropna().iloc[-1] if key in combined_summary.columns and not combined_summary[key].dropna().empty else 0 for key in comparacao_cols if key.startswith('prev acum')}
     final_real_values = {key: combined_summary[key].dropna().iloc[-1] if key in combined_summary.columns and not combined_summary[key].dropna().empty else 0 for key in comparacao_cols if key.startswith('prod acum')}
 
-    normalized_real_values = {key: (value / final_prev_values[key.replace('prod', 'prev')]) * 100 if key.replace('prod', 'prev') in final_prev_values else 0 for key, value in final_real_values.items()}
+    # Add a check to avoid division by zero
+    normalized_real_values = {key: (value / final_prev_values[key.replace('prod', 'prev')]) * 100 if key.replace('prod', 'prev') in final_prev_values and final_prev_values[key.replace('prod', 'prev')] != 0 else 0 for key, value in final_real_values.items()}
     normalized_prev_values = {key: 100 for key in final_prev_values.keys()}
-
-    final_prev_df = pd.DataFrame([
-        {'Mes': selected_mes, 'Tipo': 'Previsto', 'Produção': value, 'Serviço': key.split()[2]}
-        for key, value in normalized_prev_values.items()
-    ])
-    final_real_df = pd.DataFrame([
-        {'Mes': selected_mes, 'Tipo': 'Realizado', 'Produção': value, 'Serviço': key.split()[2]}
-        for key, value in normalized_real_values.items()
-    ])
+    final_prev_df = pd.DataFrame([ {'Mes': selected_mes, 'Tipo': 'Previsto', 'Produção': value, 'Serviço': key.split()[2]}
+                                    for key, value in normalized_prev_values.items()
+                                ])
+    final_real_df = pd.DataFrame([ {'Mes': selected_mes, 'Tipo': 'Realizado', 'Produção': value, 'Serviço': key.split()[2]}
+                                  for key, value in normalized_real_values.items()
+                                ])
     final_df = pd.concat([final_prev_df, final_real_df], ignore_index=True)
-
+    final_df['Produção (%)'] = final_df['Produção']
     final_df = pd.concat([final_prev_df, final_real_df], ignore_index=True)
 
     # Adicionar coluna de porcentagem relativa
