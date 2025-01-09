96149e193c05d3ce7a48afc05b4c64021be1c39e
Lucas jacomel <lucas.jacomel@pucpr.edu.br>
2025-01-09 15:07:12 -0300

    Update Controle_de_Produção.py

--- Controle_de_Produção.py
+++ Controle_de_Produção.py
@@ -149,6 +149,7 @@ def update_charts(selected_month, selected_services, obra_name):
     def calculate_monthly_percentage(df, total_column, total):
         last_day_of_month = df['Dias'].max()
         monthly_total = df.loc[df['Dias'] == last_day_of_month, total_column].values[0]
+        total = float(total)  # Converter total para float
         if total == 0:
             return 0
         return (monthly_total / total) * 100
